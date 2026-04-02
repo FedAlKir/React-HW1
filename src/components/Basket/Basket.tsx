d71c88654b7fddf2df8b4512626f6f33c5f64aa5
FedAlKir <alexander.cheburashkin@gmail.com>
2026-04-02 19:10:03 +0300

    restaurant groupping in basket added

--- src/components/Basket/Basket.tsx
+++ src/components/Basket/Basket.tsx
@@ -6,26 +6,36 @@ import { Dish } from "../Dish/Dish";
 import styles from "./styles.module.css"
 import { Link } from "react-router";
 import { clearBasket } from "../../store/slices/basketSlice";
+import { restaurants } from "../../materials/mocks/mockRestaurants";
+import type { DishProps } from "../../types/Dish";
 
 export const Basket: React.FC = () => {
     const dispatch = useDispatch<AppDispatch>()
     const onClick = () => { dispatch(clearBasket()) };
 
     const dishes = useSelector(selectAllDishes);
+    let restaurantsGroups: [string, DishProps[]][] = (restaurants.map(r => [r.name, r.menu.filter(d => dishes.map(dish => dish.id).includes(d.id)).map(d => d as DishProps)]) as [string, DishProps[]][]).filter(rg => rg[1].length > 0);
+    console.log(restaurantsGroups);
 
     return (
         <div>
             <div className={styles.container}>
                 <h2>Basket:</h2>
                 {
-                    dishes.length !== 0 ? dishes.map(dish => (
-                        <div>
-                            <Dish
-                            key={dish.id}
-                            id={dish.id}
-                            name={dish.name}
-                            price={dish.price}
-                            ingredients={dish.ingredients} />
+                    restaurantsGroups.length !== 0 ? 
+                    restaurantsGroups.map(([name, dishes]) => (
+                        <div className={styles.restaurantGroup}>
+                            <h2>{name}</h2>
+                            {
+                                dishes.map(dish => (
+                                    <Dish
+                                    key={dish.id}
+                                    id={dish.id}
+                                    name={dish.name}
+                                    price={dish.price}
+                                    ingredients={dish.ingredients} />
+                                ))
+                            }
                         </div>
                     )) :
                     <p className={styles.baskeEmptyLabel}>Your basket is empty</p>
