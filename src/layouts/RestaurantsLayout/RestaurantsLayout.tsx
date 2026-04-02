a3d95a50ca22615809742d8480baf36108f1b318
FedAlKir <alexander.cheburashkin@gmail.com>
2026-04-02 14:55:50 +0300

    addition commit

--- src/layouts/RestaurantsLayout/RestaurantsLayout.tsx
+++ src/layouts/RestaurantsLayout/RestaurantsLayout.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Restaurants } from "../../components/Restaurants/Restaurants";
 import { restaurants } from "../../materials/mocks/mockRestaurants";
-import { Link, Outlet } from "react-router";
+import { Outlet } from "react-router";
 import styles from "./styles.module.css"
 
 export const RestaurantsLayout = () => {
